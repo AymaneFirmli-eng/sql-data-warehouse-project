b845a40b2d8fdda65357fa30cac0039fb619d037
AymaneFirmli-eng <aymane.byzm@gmail.com>
2026-09-04 01:18:33 +0200

    Refactor SQL table creation statements

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,54 +2,58 @@
 
 
 /* CRM DATA */
-create table bronze.crm_cust_info
-(cst_id int,
-cst_key  int,
-cst_firstname VARCHAR(50),
-cst_lastname VARCHAR(50),
-cst_marital_status VARCHAR(50),
-cst_gndr VARCHAR(50),
-cst_create_date date
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE
 );
 
-create table bronze.crm_prd_info
-(prd_id int,
-prd_key  VARCHAR(50),
-prd_nm VARCHAR(50),
-prd_cost int,
-prd_line VARCHAR(50),
-prd_start_dt date,
-prd_end_dt date
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt TIMESTAMP,
+    prd_end_dt   TIMESTAMP
 );
-create table bronze.crm_sales_details
-(sls_ord_num VARCHAR(50),
-sls_prd_key VARchar(50),
-sls_cust_id int,
-sls_order_dt int,
-sls_ship_dt int,
-sls_due_dt int,
-sls_sales int,
-sls_quantity int,
-sls_price int
+
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
 );
 
 /* ERP DATA */
-create table bronze.erp_loc_a101
-(CID VARCHAR(50),
-CNTRY VARCHAR(50));
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50)
+);
 
 
-create table bronze.erp_px_cat_g1v2
-(ID VARCHAR(50),
-BDACATTE date,
-SUBCAT varchar(50),
-MAINTENANCE varchar(50));
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    VARCHAR(50),
+    bdate  DATE,
+    gen    VARCHAR(50)
+);
 
-
-create table bronze.erp_cust_az12
-(CID VARCHAR(50),
-BDATE date,
-GEN varchar(50));
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50)
+);
 
 
 
