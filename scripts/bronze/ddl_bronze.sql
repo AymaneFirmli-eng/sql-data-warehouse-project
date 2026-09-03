517c0379b6065384a250b7ae2731577155c37c1a
AymaneFirmli-eng <aymane.byzm@gmail.com>
2026-09-04 01:12:12 +0200

    Refactor SQL script with comments and table adjustments
    Added comments for naming conventions and organized table definitions.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,7 @@
+/* WE ARE USING SNAKE NAMING CONVENTION => everything is in lower case */
+
+
+/* CRM DATA */
 create table bronze.crm_cust_info
 (cst_id int,
 cst_key  int,
@@ -17,7 +21,6 @@ prd_line VARCHAR(50),
 prd_start_dt date,
 prd_end_dt date
 );
-
 create table bronze.crm_sales_details
 (sls_ord_num VARCHAR(50),
 sls_prd_key VARchar(50),
@@ -27,24 +30,37 @@ sls_ship_dt int,
 sls_due_dt int,
 sls_sales int,
 sls_quantity int,
-sls_price int,
+sls_price int
 );
 
+/* ERP DATA */
+create table bronze.erp_loc_a101
+(CID VARCHAR(50),
+CNTRY VARCHAR(50));
 
-create table bronze.erp_CUST_AZ12
+
+create table bronze.erp_px_cat_g1v2
+(ID VARCHAR(50),
+BDACATTE date,
+SUBCAT varchar(50),
+MAINTENANCE varchar(50));
+
+
+create table bronze.erp_cust_az12
 (CID VARCHAR(50),
 BDATE date,
 GEN varchar(50));
 
 
-create table bronze.erp_LOC_A101
-(CID VARCHAR(50),
-CNTRY VARCHAR(50));
 
 
 
-create table bronze.erp_PX_CAT_G1V2
-(ID VARCHAR(50),
-BDACATTE date,
-SUBCAT varchar(50)
-MAINTENANCE varchar(50);
+
+
+
+
+
+
+
+
+
